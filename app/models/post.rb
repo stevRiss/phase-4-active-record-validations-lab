fed7edebe20d24347f057f2dd8efa82324cd16a6
Stephen <stevriss22@gmail.com>
2021-11-24 11:42:58 -0500

    commit tree cfdfaeae816e0cb75e6ca457aaf4ad092be2ba87

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :forbidden
+        def forbidden(post)
+            if post.title == "True Facts"
+                errors.add(:title, "Not allowed :D")
+            
+
+            end
+
+        end
+
+
 end
